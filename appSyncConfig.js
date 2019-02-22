665eb848ff6ce02f246ae2fb6ffacbc275eec91a
brianebill <lumeo@me.com>
2019-02-21 16:55:17 -0800

    Update appSyncConfig.js

--- appSyncConfig.js
+++ appSyncConfig.js
@@ -1,8 +1,8 @@
 export default {
-  url: "https://myga5vkh3jgkxdcbmmeizoldcm.appsync-api.us-west-2.amazonaws.com/graphql",
+  url: "<ENTER_URL>",
   region: "us-west-2",
   auth: {
     type: "API_KEY",
-    apiKey: "da2-rbuocoxfqfc5hk7eoxm2jvzmz4"
+    apiKey: "<ENTER KEY>"
   }
 }
